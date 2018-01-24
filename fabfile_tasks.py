9e6a15c873404ac9fb814b8e1d13c3ecd1fd749c
Jason Chung <zzxworld@gmail.com>
2018-01-24 22:04:28 +0800

    添加数据库迁移任务

--- fabfile_tasks.py
+++ fabfile_tasks.py
@@ -10,8 +10,17 @@ def deploy():
     with cd(src_path):
         run('git checkout master && git pull')
         run('composer install')
+        run('npm run production')
         run('php artisan config:cache')
         run('php artisan cache:clear')
         run('php artisan route:clear')
         run('php artisan view:clear')
         run('php artisan queue:restart')
+
+def migrate():
+    """执行数据库迁移
+    """
+    src_path = os.path.join(env.deploy_path, 'src')
+    with cd(src_path):
+        run('git checkout master && git pull')
+        run('php artisan migrate')
