27e7b2f7247072e9e166d49ce371abac032bd46a
Jason Chung <zzxworld@gmail.com>
2018-01-28 16:01:35 +0800

    文本模型添加内容切分方法

--- app/Models/Text.php
+++ app/Models/Text.php
@@ -9,8 +9,23 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Text extends Model
 {
+    protected $fillable = ['text'];
+
     public function textable()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * 按指定字数切分文本内容
+     */
+    public static function segment($content, $limit=1023)
+    {
+        $segments = collect();
+        $total = ceil(mb_strlen($content)/$limit);
+        foreach (range(0, $total-1) as $pos) {
+            $segments->push(mb_substr($content, $pos*$limit, $limit));
+        }
+        return $segments;
     }
 }
