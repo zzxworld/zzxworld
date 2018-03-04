b1faac3aa3ab93aaea06152a183652dd6964b8c4
Jason Chung <zzxworld@gmail.com>
2018-03-04 15:30:11 +0800

    文章评论支持匿名和注册用户

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -10,11 +10,17 @@ class PostCommentController extends Controller
 {
     public function store(Request $request, Post $post)
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|email',
+        $rules = [
             'content' => 'required',
-        ]);
+        ];
+
+        $user = $request->user();
+        if (!$user) {
+            $rules['name'] = 'required';
+            $rules['email'] = 'required|email';
+        }
+
+        $this->validate($request, $rules);
 
         $comment = $post->comments()->create([
             'user_id' => $request->user() ? $request->user()->id : 0,
