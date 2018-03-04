733aa90d35b1ccc60d28ee3ec19de7186359f674
Jason Chung <zzxworld@gmail.com>
2018-03-04 17:30:27 +0800

    部署命令添加 npm 依赖命令处理

--- fabfile_tasks.py
+++ fabfile_tasks.py
@@ -10,6 +10,7 @@ def deploy():
     with cd(src_path):
         run('git checkout master && git pull')
         run('composer install')
+        run('npm install')
         run('npm run production')
         run('php artisan config:cache')
         run('php artisan cache:clear')
