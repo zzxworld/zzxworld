7ffea71b291f6571e2eb3a919f3ffed06b984b2e
Jason Chung <zzxworld@gmail.com>
2018-03-18 16:20:55 +0800

    添加新闻源的控制器方法和模版文件

--- app/Http/Controllers/News/FeedController.php
+++ app/Http/Controllers/News/FeedController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\News;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\NewsFeed;
 
 class FeedController extends Controller
 {
-    //
+    public function index()
+    {
+        $feeds = NewsFeed::orderBy('updated_at', 'desc')->get();
+
+        return view('news.feeds.index', ['feeds' => $feeds]);
+    }
+
+    public function create()
+    {
+        $feed = new NewsFeed;
+
+        return view('news.feeds.create', ['feed' => $feed]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate([
+            'name' => 'required',
+            'url' => 'required|url',
+        ]);
+
+        $feed = NewsFeed::create($request->all());
+
+        return redirect('news/feeds');
+    }
+
+    public function edit(NewsFeed $feed)
+    {
+        return view('news.feeds.edit', ['feed' => $feed]);
+    }
+
+    public function destory(NewsFeed $feed)
+    {
+        $feed->destroy();
+
+        return redirect('news/feeds');
+    }
 }
