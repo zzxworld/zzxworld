8179f7a8787af193de606975907f94138c8fa98b
Jason Chung <zzxworld@gmail.com>
2018-03-19 22:31:27 +0800

    新闻详情显示时间

--- resources/views/news/posts/show.blade.php
+++ resources/views/news/posts/show.blade.php
@@ -8,6 +8,7 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h4 class="panel-title">{{ $post->title }}</h4>
+                <span>{{ $post->created_at->format('Y-m-d') }}</span>
             </div>
             <div class="panel-body">{!! $post->text !!}</div>
             <div class="panel-footer">
