0893f19954fb4d0791f1a5cf7637ec90a1a38693
Jason Chung <zzxworld@gmail.com>
2018-03-20 22:47:42 +0800

    添加 Atom 格式 RSS 解析功能

---
+++ app/Console/Commands/FetchNews.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\NewsFeed;
+
+class FetchNews extends Command
+{
+    protected $signature = 'news:fetch {--id=}';
+    protected $description = 'fetch news';
+
+    public function handle()
+    {
+        $id = (int) $this->option('id');
+
+        if ($id) {
+            $feeds = NewsFeed::where('id', $id)->get();
+        } else {
+            $feeds = NewsFeed::all();
+        }
+
+        foreach ($feeds as $feed) {
+            $feed->fetchAndSave();
+        }
+    }
+}
--- app/Models/NewsFeed.php
+++ app/Models/NewsFeed.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Requests;
 use Carbon\Carbon;
+use Log;
 
 class NewsFeed extends Model
 {
@@ -21,10 +22,13 @@ class NewsFeed extends Model
     public function fetch()
     {
         $response = Requests::get($this->url);
-
         $xml = simplexml_load_string($response->body);
-        if ($xml->getName() == 'rss') {
+        $type = $xml->getName();
+
+        if ($type == 'rss') {
             $items = static::parseRSS($xml);
+        } else if ($type == 'feed') {
+            $items = static::parseAtom($xml);
         } else {
             $items = collect();
         }
@@ -61,20 +65,58 @@ class NewsFeed extends Model
     }
 
     /**
+     * 解析 Atom 格式数据
+     */
+    public static function parseAtom(\SimpleXMLElement $xml)
+    {
+        $data = collect();
+
+        $namespace = $xml->getNamespaces();
+        $xml->registerXPathNamespace('c', array_values($namespace)[0]);
+
+        $items = $xml->xpath('//c:entry');
+        foreach ($items as $item) {
+            $createdAt = strtotime((string) $item->published);
+            if (!$createdAt) {
+                continue;
+            }
+
+            $title = (string) $item->title;
+            $url = (string) $item->link->xpath('@href')[0];
+            $content = $item->content->xpath('./*')[0]->asXML();
+
+            $data->push([
+                'title' => $title,
+                'url' => $url,
+                'created_at' => date('Y-m-d H:i:s', $createdAt),
+                'description' => $content,
+            ]);
+        }
+
+        return $data;
+    }
+
+    /**
      * 获取并保存新闻源数据
      */
     public function fetchAndSave()
     {
         $items = $this->fetch();
         $exists = $this->posts()->whereIn('sign', $items->pluck('sign'))->pluck('sign');
-        $items = $items->whereNotIn('sign', $exists);
 
         foreach ($items as $rs) {
+            if ($exists->search($rs['sign']) !== false) {
+                continue;
+            }
+
             $rs['news_feed_id'] = $this->id;
+
             $post = NewsPost::create($rs);
             if ($post) {
                 $post->saveText($rs['description']);
             }
+
+            $exists->push($rs['sign']);
         }
     }
 }
