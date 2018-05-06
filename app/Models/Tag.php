1ff4d7ab06219ba62b973134b5fde087732b8a84
Jason Chung <zzxworld@gmail.com>
2018-05-06 13:47:48 +0800

    添加标签批量查找和添加功能

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public static function bulkFindOrCreate(array $names)
+    {
+        $tags = collect();
+        $exists = static::whereIn('name', $names)->get();
+
+        foreach ($names as $name) {
+            $tag = $exists->where('name', $name)->first();
+            if (!$tag) {
+                $tag = static::create([
+                    'name' => $name,
+                ]);
+            }
+            $tags->push($tag);
+        }
+
+        return $tags;
+    }
 }
---
+++ tests/Unit/TagTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Tag;
+
+class TagTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testBulkFindOrCreate()
+    {
+        $names = [];
+
+        // 空元素测试
+        $tags = Tag::bulkFindOrCreate($names);
+        $this->assertEquals(collect(), $tags);
+
+        // 正常测试
+        $names = ['aaa', 'bbb', 'ccc'];
+        $tags = Tag::bulkFindOrCreate($names);
+        $this->assertEquals(3, $tags->count());
+        $this->assertEquals($names, $tags->pluck('name')->toArray());
+
+        // 重复测试
+        $tags = Tag::bulkFindOrCreate($names);
+        $this->assertEquals(3, $tags->count());
+    }
+}
