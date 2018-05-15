68af72bf3686a12a23e50f2160770f352bce98ee
Jason Chung <zzxworld@gmail.com>
2018-05-15 21:25:05 +0800

    显示加密传输网站链接

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -2,8 +2,15 @@
 
 @section('title', '网站 | zzxworld')
 
-@section('content')
+@push('css')
+    <style type="text/css" media="screen">
+        .site-item .https {
+            color: #090;
+        }
+    </style>
+@endpush
 
+@section('content')
     <div class="container">
         <div class="panel panel-default">
             <div class="panel-heading">
@@ -12,6 +19,9 @@
             <ul class="list-group">
                 @foreach ($sites as $site)
                     <li class="list-group-item site-item">
+                        @if ($site->scheme == 'https')
+                            <span class="glyphicon glyphicon-lock https"></span>
+                        @endif
                         <a class="link" rel="external nofollow" target="_blank" href="{{ url('sites/'.$site->id.'/go') }}">{{ $site->name }}</a>
                     </li>
                 @endforeach
@@ -19,5 +29,4 @@
         </div>
         {{ $sites->links() }}
     </div>
-
 @endsection
